b1cf03e689e22a784ca8af4f7dfd458cf574f7eb
sweetyBhaai <sweetynakashe@gmail.com>
2025-03-26 13:48:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ function allowDrop(event) {
 }
 
 function drag(event) {
-    event.dataTransfer.setData("text", event.target.innerText);
+    event.dataTransfer.setData("text", event.target.id);
 }
 
 function drop(event) {
     event.preventDefault();
     let data = event.dataTransfer.getData("text");
-    let newElement = document.createElement("div");
-    newElement.innerText = data;
-    newElement.style.padding = "10px";
-    newElement.style.margin = "5px";
-    newElement.style.border = "1px solid black";
-    event.target.appendChild(newElement);
+    let draggedElement = document.getElementById(data);
+    
+    if (draggedElement) {
+        let newElement = draggedElement.cloneNode(true);
+        newElement.removeAttribute("id");
+        newElement.style.position = "absolute";
+        newElement.style.left = event.clientX + "px";
+        newElement.style.top = event.clientY + "px";
+        document.getElementById("canvas").appendChild(newElement);
+    }
 }
 
+// Attach drag event to elements dynamically
 document.querySelectorAll(".draggable").forEach(item => {
+    item.setAttribute("id", "drag-" + Math.random().toString(36).substr(2, 9)); 
+    item.setAttribute("draggable", "true");
     item.addEventListener("dragstart", drag);
 });
